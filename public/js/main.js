e826acc8791a132d67082202cb197ffd2fbebe82
Luis Enrique Garcia <legarcia2904@gmail.com>
2017-12-04 07:45:50 -0800

    Funciona el mapa en la pagina principal

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,16 @@ angular.module('rentuiApp',['ngRoute','controladores.rentui', 'ngMap']).config(c
 function configurar($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl:'/templates/main.html'
+            templateUrl:'/templates/main.html',
+            controller:'CasaController',
+            resolve:{
+                casasPromise: ['Casa', function (Casa) {
+                    Casa.getCasas();
+                }],
+                arrendadoresPromise: ['Arrendador', function (Arrendador) {
+                    Arrendador.getArrendadores();
+                }]
+            }
         })
         .when('/casas', {
             templateUrl:'/templates/casas.html',
